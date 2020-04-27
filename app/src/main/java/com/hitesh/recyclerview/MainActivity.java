b18703429f44f1106a8a2e97276d335aa697356c
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-27 22:28:35 +0530

    Initial Push

---
+++ app/src/main/java/com/hitesh/recyclerview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hitesh.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<items> itemsList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mAdapter = new RecyclerViewAdapter(itemsList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        addItems();
+    }
+
+
+
+    private void addItems() {
+        items items = new items("title ", "title2", "title3");
+        itemsList.add(items);
+
+
+
+
+
+       // mAdapter.notifyDataSetChanged();
+    }
+}
